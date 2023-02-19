f97b8020e9186df777e3f5b11b0246002f82ac76
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-19 12:43:58 -0500

    set up express server security

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  isProduction: process.env.NODE_ENV === 'production'
 }
 
 console.log('MongoDB URI:', process.env.MONGO_URI);
